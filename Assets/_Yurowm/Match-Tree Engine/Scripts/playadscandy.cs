93639ef0612cbe83fe46317618225d537f0ab2c9
waqasnu <waqasnu>
2016-11-08 15:06:41 +0500

    sweet

---
+++ Assets/_Yurowm/Match-Tree Engine/Scripts/playadscandy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using System.Collections;
+
+public class playadscandy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("LaunchProjectile", 180,180);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LaunchProjectile () {
+
+		Advertisement.Show ();
+
+	}
+}
